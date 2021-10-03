62532307c552733477c1fb5e6f3702f2b8c98f69
Samciucov Valentin <90217118+SamValentin7@users.noreply.github.com>
2021-10-03 18:27:52 +0300

    Add files via upload

---
+++ Project02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n=9;
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n-i+1; j++)
+        {
+            cout<<(char)(n-i+j+64)<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
